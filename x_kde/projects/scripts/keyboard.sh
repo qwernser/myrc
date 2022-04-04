7d19b94d20148a2f65eb850335278099ac7c92cf
Richard Yi <richard20925@gmail.com>
2022-04-05 04:10:18 +0800

    Increase wait time

--- x_kde/projects/scripts/keyboard.sh
+++ x_kde/projects/scripts/keyboard.sh
@@ -7,6 +7,6 @@ function remap() {
 if [ -z $1 ] ; then remap 6 ; fi
 inotifywait -e modify /sys/devices/virtual/tty/tty0/active
 if [ "$(cat /sys/devices/virtual/tty/tty0/active)" = "tty7" ] ; then
-    remap 1
+    remap 3
 fi
 exec $0 non-initial
