aaafa8f7b45defc5be0c9ea8f3b0f1ac538c671e
Richard Yi <richard20925@gmail.com>
2024-02-11 19:13:50 +0800

    Fix issue from disabling snippet

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -121,6 +121,7 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities.textDocument.completion.completionItem.snippetSupport = false
 
 local servers = {
     'pyright', -- pyright
